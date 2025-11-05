ec6d8d5a7e4d726dc8fa459fb59d1a15e444dde9
Ajayakumar Mendigeri <ajaymendigeri@gmail.com>
2025-11-05 15:59:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+
+---
+
+## 🎯 Purpose
+To reduce energy usage in advertisement displays and promote **eco-friendly and sustainable smart-display systems**.
+
+---
+
+## 📌 Future Enhancements
+- Cloud-based remote ad update system
+- Weather-proof outdoor model
+- IoT & real-time data integration
+
+---
+
+## 👤 Author
+**Ajay Mendigeri**
